fc65bd61bdee4185538fd5d8ff3b3f0b0f483b8d
Dario Izzo <dario.izzo@gmail.com>
2014-11-13 15:13:02 +0100

    pep8 fixes and private members added

--- python/dnn_control/test.py
+++ python/dnn_control/test.py
@@ -7,21 +7,24 @@ from scipy.integrate import odeint
 TIME = 100000.0
 SAMPLING_FREQUENCY = 10.0
 
-INERTIA_X = 4567.0 # [kg*m^2]
-INERTIA_Y = 2345.0 # [kg*m^2]
-INERTIA_Z = 1234.0 # [kg*m^2]
-DENSITY = 2000.0 # [kg/m^3]
-ANGULAR_VELOCITY = [0.0005,0.0007,-0.0003] # [1/s]
-TIME_BIAS = 0.0 # [s]
+INERTIA_X = 4567.0  # [kg*m^2]
+INERTIA_Y = 2345.0  # [kg*m^2]
+INERTIA_Z = 1234.0  # [kg*m^2]
+DENSITY = 2000.0  # [kg/m^3]
+ANGULAR_VELOCITY = [0.0005, 0.0007, -0.0003]  # [1/s]
+TIME_BIAS = 0.0  # [s]
 
-asteroid = Asteroid(INERTIA_X, INERTIA_Y, INERTIA_Z, DENSITY, ANGULAR_VELOCITY, TIME_BIAS)
+asteroid = Asteroid(
+    INERTIA_X, INERTIA_Y, INERTIA_Z, DENSITY, ANGULAR_VELOCITY, TIME_BIAS)
+
 
 def w_dot(state, time, inertia_x, inertia_y, inertia_z):
     return [(inertia_z - inertia_y) * state[2] * state[1] / inertia_x, (inertia_x - inertia_z) * state[0] * state[2] / inertia_y, (inertia_y - inertia_x) * state[1] * state[0] / inertia_z]
 
-test_time = 132.32
+test_time = 12.32
 
-result = odeint(w_dot, ANGULAR_VELOCITY, [0, test_time], (INERTIA_X, INERTIA_Y, INERTIA_Z))
+result = odeint(
+    w_dot, ANGULAR_VELOCITY, [0, test_time], (INERTIA_X, INERTIA_Y, INERTIA_Z))
 omega_odeint = result[1][:]
 omega_analytical = asteroid.angular_velocity_at_time(test_time)
 print("{0} {0}".format(omega_odeint, omega_analytical))
@@ -31,9 +34,9 @@ iterations = int(TIME*SAMPLING_FREQUENCY)
 
 angular_velocity = empty([iterations,3])
 for i in range(iterations):
-	time = i*1.0/SAMPLING_FREQUENCY
-	omega = asteroid.angular_velocity_at_time(time)
-	angular_velocity[i][:] = omega
+    time = i*1.0/SAMPLING_FREQUENCY
+    omega = asteroid.angular_velocity_at_time(time)
+    angular_velocity[i][:] = omega
 
 # Visualize trajectory
 fig = pyplot.figure()
