7452fc0d3f283e79cc4f15243880545c8e3ce43f
Stefan Willi <willist@student.ethz.ch>
2015-02-26 23:16:57 +0100

    Bump.

--- cpp/dnn_control/main.cpp
+++ cpp/dnn_control/main.cpp
@@ -24,14 +24,14 @@ int main(int argc, char *argv[]) {
     //TestLeastSquaresPolicyController(9782);
     //return 0;
 
-    //TrainNeuralNetworkController();
-    //return 0;
+    TrainNeuralNetworkController();
+    return 0;
 
     //TrainProportionalDerivativeController();
     //return 0;
 
-    TrainLeastSquaresPolicyController();
-    return 0;
+    //TrainLeastSquaresPolicyController();
+    //return 0;
 
     //SensorDataGenerator generator(PATH_TO_SENSOR_DATA_FOLDER, 24.0 * 60.0 * 60.0);
     //generator.Generate(100, rand());
