c2347d38afb9a8a38559d77a31c342866af9be0e
Stefan Willi <stefan.j.willi@gmail.com>
2015-05-23 17:30:51 +0200

    Clean up code.

--- python/dnn_control/fullstatesensorsimulator.py
+++
@@ -1,9 +0,0 @@
-class FullStateSensorSimulator:
-
-    def __init__(self, asteroid, sensor_noise):
-        self._asteroid = asteroid
-        self._sensor_noise = sensor_noise
-
-    # Generates (simulates) sensor data based on the current spacecraft state "state" and time "time"
-    def simulate(self, state, perturbations_acceleration, time):
-        return state
